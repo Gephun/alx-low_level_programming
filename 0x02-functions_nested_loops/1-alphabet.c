97e7b5bf74dc60d11fd821f8508cd3a2e51ad5b9
Gephun <gephunonyango@gmail.com>
2022-07-06 00:39:11 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet - check the code for Holberton School students.
+ * print_alphabet -> prints the lowercase alphabets
  *
  * Return: Always 0.
  */
