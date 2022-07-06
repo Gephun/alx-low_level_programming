d5988cba14c045f09b01777cdf1306c6c9610595
Gephun <gephunonyango@gmail.com>
2022-07-06 00:43:16 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - func
+ * print_alphabet_x10 - function that printd 10 times the alphabet in lowercase
  *
  * Return - 0
  */
