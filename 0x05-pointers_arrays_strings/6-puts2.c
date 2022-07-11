260724037304a17f3c936bda7c0e65fbb211a35d
Gephun <gephunonyango@gmail.com>
2022-07-10 22:39:09 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,9 @@
 void puts2(char *str)
 {
 	int len, i;
+
 	len = strlen(str);
-	for (i = 0; i < len; i+= 2)
-		-putchar(str[i]);
-	-putchar('\n');
+	for (i = 0; i < len; i+ =  2)
+		_putchar(str[i]);
+	_putchar('\n');
 }
